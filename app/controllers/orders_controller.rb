0e7cb5f66651ad303db012a74a0d635d9faad022
Guido Rombolá <guidorombola@gmail.com>
2019-03-11 16:45:10 -0300

    Se muestra el monto a pagar por persona al finalizar el pedido

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,36 +5,88 @@ class OrdersController < ApplicationController
 
   def newOrder
     @place = Place.find(params[:id])
+    session[:place] = @place
   end
 
   def create
+    session[:order] = params[:order]
+    session[:order].permit!
     total_order
     order_by_person
+    #clean_others_entry
+  end
+
+  def finish
+    @total_order_by_person = session[:order]['total_order_by_person']
+    @total_order = session[:order]['total_order']
+    @sum_of_varieties_by_person = session[:order]['sum_of_varieties_by_person']
+    @sum_of_all_varieties = session[:order]['sum_of_all_varieties']
+    @price = params[:price]['price'].to_i
+    @price_by_person = {}
+    price_per_unit = @price/@sum_of_all_varieties
+    @sum_of_varieties_by_person.each do |person, quantity|
+      @price_by_person[person] = price_per_unit * @sum_of_varieties_by_person[person]
+    end
   end
 
   private
 
   def order_by_person
-    @total_order_by_person = {}
-    params[:order][:q].to_h.map do |variety_id, order_for_variety|
+    @total_order_by_person = Hash.new(0)
+    @sum_of_varieties_by_person = Hash.new(0)
+    session[:order][:q].to_h.map do |variety_id, order_for_variety|
+      #params[:order][:q].to_h.map do |variety_id, order_for_variety|
       variety_name = Variety.find(variety_id).name
-      @total_order_by_person[variety_name] = {}
-      order_for_variety.each do |persona, cantidad|
-        @total_order_by_person[variety_name][params[:order]['name'][persona]] = cantidad.to_i
+      @total_order_by_person[variety_name] = Hash.new(0)
+      order_for_variety.each do |person, quantity|
+        person_name = params[:order]['name'][person]
+        if person_name == ''
+          person_name = 'Otros'
+        end
+        @total_order_by_person[variety_name][person_name] += quantity.to_i
+        @sum_of_varieties_by_person[person_name] += quantity.to_i
       end
     end
+    session[:order][:total_order_by_person] = @total_order_by_person
+    session[:order][:sum_of_varieties_by_person] = @sum_of_varieties_by_person
   end
 
   def total_order
-    params.require(:order).permit!
+
+    #params.require(:order).permit!
     @total_order = []
+    @sum_of_all_varieties = 0
     #params[:order][:q].to_h.map {|k, v| totales_por_empanada[k.to_i] = v.values.map {|c| c.to_i}.sum}
-    params[:order][:q].to_h.map do |variety_id, order_for_variety|
+    #params[:order][:q].to_h.map do |variety_id, order_for_variety|
+
+    session[:order][:q].to_h.map do |variety_id, order_for_variety|
       total_per_variety = {}
       total_per_variety['variety'] = Variety.find(variety_id)
       total_per_variety['quantity'] = order_for_variety.values.map {|c| c.to_i}.sum
+      @sum_of_all_varieties += total_per_variety['quantity']
       @total_order << total_per_variety
     end
+    session[:order][:total_order] = @total_order
+    session[:order][:sum_of_all_varieties] = @sum_of_all_varieties
   end
 
+  # todo ffv
+  def clean_others_entry
+    others_has_orders = false
+    @total_order_by_person.each do |variety, order_by_person|
+      order_by_person.each do |name, quantity|
+        if name == 'Otros' and quantity != 0
+          others_has_orders = true
+        end
+      end
+    end
+    unless others_has_orders
+      @total_order_by_person.each do |variety, order_by_person|
+        order_by_person.each do |name, quantity|
+          @total_order_by_person[variety].delete name
+        end
+      end
+    end
+    @total_order_by_person
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :varieties
   resources :orders, only: [:index, :new, :create]
   get '/orders/new/:id', to: 'orders#newOrder', as: "newOrder"
-  post '/orders/new/:id', to: 'orders#create'
+  post '/orders/create', to: 'orders#create', as: "create_order"
   get '/orders/show_order', as: "show_order"
+  post '/orders/finish', to: 'orders#finish', as: 'finish_order'
 end
