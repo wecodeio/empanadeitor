be1f101d7f4335ca06b5911f0a32b30c1d6eb5e1
Guido Rombolá <guidorombola@gmail.com>
2019-03-12 11:27:30 -0300

    Se reutiliza la vista del create para el finish por medio de partials

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -17,60 +17,55 @@ class OrdersController < ApplicationController
   end
 
   def finish
-    @total_order_by_person = session[:order]['total_order_by_person']
-    @total_order = session[:order]['total_order']
-    @sum_of_varieties_by_person = session[:order]['sum_of_varieties_by_person']
-    @sum_of_all_varieties = session[:order]['sum_of_all_varieties']
-    @price = params[:price]['price'].to_i
-    @price_by_person = {}
-    price_per_unit = @price/@sum_of_all_varieties
-    @sum_of_varieties_by_person.each do |person, quantity|
-      @price_by_person[person] = price_per_unit * @sum_of_varieties_by_person[person]
+    price = params[:price]['price'].to_i
+    price_by_person = {}
+    sum_of_all_varieties = session[:order]['sum_of_all_varieties']
+    sum_of_varieties_by_person = session[:order]['sum_of_varieties_by_person']
+    price_per_unit = price / sum_of_all_varieties
+    sum_of_varieties_by_person.each do |person, quantity|
+      price_by_person[person] = price_per_unit * sum_of_varieties_by_person[person]
     end
+    session[:order]['price_by_person'] = price_by_person
   end
 
   private
 
   def order_by_person
-    @total_order_by_person = Hash.new(0)
-    @sum_of_varieties_by_person = Hash.new(0)
+    total_order_by_person = Hash.new(0)
+    sum_of_varieties_by_person = Hash.new(0)
     session[:order][:q].to_h.map do |variety_id, order_for_variety|
-      #params[:order][:q].to_h.map do |variety_id, order_for_variety|
       variety_name = Variety.find(variety_id).name
-      @total_order_by_person[variety_name] = Hash.new(0)
+      total_order_by_person[variety_name] = Hash.new(0)
       order_for_variety.each do |person, quantity|
         person_name = params[:order]['name'][person]
         if person_name == ''
           person_name = 'Otros'
         end
-        @total_order_by_person[variety_name][person_name] += quantity.to_i
-        @sum_of_varieties_by_person[person_name] += quantity.to_i
+        total_order_by_person[variety_name][person_name] += quantity.to_i
+        sum_of_varieties_by_person[person_name] += quantity.to_i
       end
     end
-    session[:order][:total_order_by_person] = @total_order_by_person
-    session[:order][:sum_of_varieties_by_person] = @sum_of_varieties_by_person
+    session[:order][:total_order_by_person] = total_order_by_person
+    session[:order][:sum_of_varieties_by_person] = sum_of_varieties_by_person
   end
 
   def total_order
-
-    #params.require(:order).permit!
-    @total_order = []
-    @sum_of_all_varieties = 0
-    #params[:order][:q].to_h.map {|k, v| totales_por_empanada[k.to_i] = v.values.map {|c| c.to_i}.sum}
-    #params[:order][:q].to_h.map do |variety_id, order_for_variety|
+    total_order = []
+    sum_of_all_varieties = 0
 
     session[:order][:q].to_h.map do |variety_id, order_for_variety|
       total_per_variety = {}
       total_per_variety['variety'] = Variety.find(variety_id)
       total_per_variety['quantity'] = order_for_variety.values.map {|c| c.to_i}.sum
-      @sum_of_all_varieties += total_per_variety['quantity']
-      @total_order << total_per_variety
+      sum_of_all_varieties += total_per_variety['quantity']
+      total_order << total_per_variety
     end
-    session[:order][:total_order] = @total_order
-    session[:order][:sum_of_all_varieties] = @sum_of_all_varieties
+    session[:order][:total_order] = total_order
+    session[:order][:sum_of_all_varieties] = sum_of_all_varieties
   end
 
-  # todo ffv
+  # todo
+=begin
   def clean_others_entry
     others_has_orders = false
     @total_order_by_person.each do |variety, order_by_person|
@@ -89,4 +84,5 @@ class OrdersController < ApplicationController
     end
     @total_order_by_person
   end
+=end
 end
