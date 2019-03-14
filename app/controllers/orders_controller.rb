b18a7a9b579e919db9f7ce1bd85ca0706ca03ab7
Abel Fabian Lagorio <falago@gmail.com>
2019-03-14 11:06:46 -0300

    Se traslada logica del controlador order a la clase order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,12 +12,9 @@ class OrdersController < ApplicationController
     params[:order].permit!
     session[:input_user] = params[:order].to_h
     @order = fill_order
-    save_list_of_participants
-    
   end
 
   def finish
-    @price_by_person = {}
     @order = fill_order
     @order.price = params[:price]['price'].to_i
   end
@@ -31,21 +28,13 @@ class OrdersController < ApplicationController
       person_name = session[:input_user]['name'][person_id]
       varieties_chosen.map do |variety_id, quantity|
         variety_name = (Variety.find(variety_id)).name
-        if quantity.to_i > 0
+        if quantity.to_i > 0 and !person_name.empty?
           order_detail = OrderDetail.new(person_name, variety_name, quantity.to_i)
           order.add_order_detail(order_detail)
-          varieties_to_buy << variety_name
         end
       end
     end
-    session[:varieties_to_buy] = varieties_to_buy.uniq
     order
-  end
-
-  def save_list_of_participants
-    participants = params[:order][:name].to_h.values
-    participants.reject! { |p| p.empty? }
-    session[:order_participants] = participants
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -9,7 +9,7 @@ class Order
   end
 
   def add_order_detail(order_detail)
-    order_details << order_detail
+      @order_details << order_detail
   end
 
   def total_units_per_variety
@@ -27,7 +27,7 @@ class Order
   def total_units_per_person
     total_units_per_person = Hash.new(0)
     @order_details.each do |detail|
-      total_units_per_person[detail.person] += detail.quantity
+        total_units_per_person[detail.person] += detail.quantity
     end
     total_units_per_person
   end
@@ -35,8 +35,8 @@ class Order
   def price_per_person
     load_prices
     price_per_person = Hash.new(0)
-    order_details.each do |detail|
-      price_per_person[detail.person] += detail.price
+    @order_details.each do |detail|
+        price_per_person[detail.person] += detail.price
     end
     price_per_person
   end
@@ -46,6 +46,22 @@ class Order
     order_details.each do |detail|
       detail.price = detail.quantity * price_per_unit
     end
+  end
+
+  def list_of_participants
+    participants = []
+    @order_details.each do |detail|
+        participants << detail.person
+    end 
+    participants = participants.uniq
+  end
+
+  def  varieties_to_buy 
+    varieties_to_buy =[]
+    @order_details.each do |detail|
+      varieties_to_buy << detail.variety
+    end
+    varieties_to_buy = varieties_to_buy.uniq
   end
 
 end
