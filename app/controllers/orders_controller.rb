b41caba314741bf3be71a75130082c6b59354f7a
Abel Fabian Lagorio <falago@gmail.com>
2019-03-14 15:18:26 -0300

    Se juntan todos los pedidos de empanadas sin nombre, en la columna others

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -28,9 +28,13 @@ class OrdersController < ApplicationController
       person_name = session[:input_user]['name'][person_id]
       varieties_chosen.map do |variety_id, quantity|
         variety_name = (Variety.find(variety_id)).name
-        if quantity.to_i > 0 and !person_name.empty?
-          order_detail = OrderDetail.new(person_name, variety_name, quantity.to_i)
-          order.add_order_detail(order_detail)
+        if quantity.to_i > 0 
+          unless person_name.empty?
+            order_detail = OrderDetail.new(person_name, variety_name, quantity.to_i)
+          else
+            order_detail = OrderDetail.new('Others', variety_name, quantity.to_i)
+          end  
+            order.add_order_detail(order_detail)
         end
       end
     end
