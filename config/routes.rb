9867a4aa42160572cd272158b499834ef08c4cd5
Guido Rombolá <guidorombola@gmail.com>
2019-04-05 15:22:25 -0300

    Elimina rutas innecesarias

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,7 @@ Rails.application.routes.draw do
   end
   resources :orders, except: [:delete] do
     get :new_custom_place, on: :collection, to: 'orders#new_custom_place'
-    get :confirm, on: :member
     post :finish, on: :member
-    get :join, on: :member
     post :create_join, on: :member
     post :send_order_slug, on: :collection
   end
